d9950f87bd214c4611dd19f0404eb9fb52400f95
tomasbrown1 <tombrown1@gmail.com>
2015-08-05 19:35:43 +0100

    worked more on desktop responsivness, added search bar

--- amarettosour.php
+++ amarettosour.php
@@ -4,22 +4,28 @@ $pageDescription = 'How to make an Ammaretto Sour';
 include ('includes/header.html')
 ?>
 
-				<img src="####################" alt="Amaretto Sour">
+				<img src="http://www.10thkitchen.com/wp-content/uploads/2013/02/DSC_0127.jpg" alt="Amaretto Sour" class="photo">
 				<h2>Name:</h2>
 				<p>Amaretto Sour</p>
 				<h2>Glassware:</h2>
 				<p>Old Fashioned</p>
 				<h2>Ingredients:</h2>
-				<p>50ml Amaretto<br />
-					25ml Lemon Juice<br />
-					15ml Sugar Syrup<br />
-					1 Egg White<br />
-					Bitters<br />
-					Slices of Lemon<br />
-					Cherry</p>
+				<ul>
+					<li>50ml Amaretto</li>
+					<li>25ml Lemon Juice</li>
+					<li>15ml Sugar Syrup</li>
+					<li>1 Egg White</li>
+					<li>Bitters</li>
+					<li>Slices of Lemon</li>
+					<li>Cherry</li>
+				</ul>
 				<h2>Method:</h2>
-				<p>Whisk the egg white up in the boston with a bar spoon until it’s foamy and full of air. Add the amaretto, lemon juice, sugar syrup and a couple drops of bitters 
-					and shake vigorously. Strain into an Old Fashioned glass full of ice and garnish with the lemon and cherry.</p>
+				<ul>
+					<li>Whisk the egg white up in the boston with a bar spoon until it’s foamy and full of air</li> 
+					<li>Add the amaretto, lemon juice, sugar syrup and bitters and shake vigorously</li> 
+					<li>Strain into an Old Fashioned glass full of ice</li>
+					<li>garnish with the lemon and cherry</li>
+				</ul>
 				<h2>History:</h2>
 				<p>A cocktail from the ‘Sours’ category</p>
 				<h2>Variations:</h2>
--- js/js.js
+++ js/js.js
@@ -1,11 +1,22 @@
 $(function() {
 	var d = new Date()
 	var $navButton = $("#menu");
+	var $searchButton = $("#search");
 	var $nav = $("#nav");
+	var $search = $("#searchbar");
 
 	document.getElementById("year").innerHTML = (d.getFullYear());
 
 	$navButton.click(function(){
+		if ($(window).width() < 900) {
+			$search.slideUp(300);
+		}
 		$nav.slideToggle(300);
 	});
+
+	$searchButton.click(function(){
+		$nav.slideUp(300);
+		$search.slideToggle(300);
+	});
+
 });
