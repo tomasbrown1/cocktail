9f00110a7e9e6af8bfb7cc4ce43b2f3680021981
tomasbrown1 <tombrown1@gmail.com>
2015-10-03 12:52:06 +0100

    Updated all content minus photos, added adsense ads and search

---
+++ search.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle = 'Drink Wizard';
+$pageDescription = 'Drink Wizard ome age | How to make expert cocktails';
+include ('includes/header.html')
+?>
+
+
+				<div id="cse-search-results"></div>
+				<script type="text/javascript">
+				  var googleSearchIframeName = "cse-search-results";
+				  var googleSearchFormName = "cse-search-box";
+				  var googleSearchFrameWidth = "100%";
+				  var googleSearchDomain = "www.google.co.uk";
+				  var googleSearchPath = "/cse";
+				</script>
+				<script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
+
+
+
+<?php
+include ('includes/footer.html')
+?>
